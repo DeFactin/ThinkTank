820ecf0f6062ad9e5932a8144782fef42b2d0be9
DeFactin <aminadefacto47@gmail.com>
2024-04-29 10:33:21 +0200

    Initial Commit
    MERN Stack Setup

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/')
+      .then(res => res.text())
+      .then(data => setMessage(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello from MERN stack!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/ThinkTank', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB.');
+}).catch(error => {
+  console.error(error);
+});
+
