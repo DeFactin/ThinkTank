e00ea1c4567ced9e608ad62e894c21e340c04681
Lamija <lamija2002@gmail.com>
2024-05-19 21:54:42 +0200

    Home Page Frontend Look

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,49 @@
 const Home = () => {
 
     return (
-        <div className="home">
-            <h2>Home</h2>
+        <div>
+            <div className="home">
+                <h1 className="Heading">ThinkTank</h1>
+                <div className="Wrapper">
+                    <div className="Intro-Card2">
+                        <div className="Intro-Card2-Header">
+                            <img className="PersonImage"
+                                src="../Person.PNG"
+                                alt="Person"
+                                width={150}
+                            />
+                            <h3>Hana Hannani</h3>
+                        </div>
+                        <p>"ThinkTank has transformed my learning journey. As a student, finding
+                            engaging study methods was always a challenge. But since discovering
+                            ThinkTank, I've found myself eagerly diving into quizzes on various topics
+                            every day."</p>
+                    </div>
+                    <img className="Chestimage"
+                        src="../Chest.PNG"
+                        alt="Chest"
+                        width={250}
+                    />
+                    <div className="Intro-Card">
+                        <p>Welcome to our quiz app, where knowledge meets fun!
+                            Whether you're a seasoned learner or just starting your educational journey,
+                            our platform offers a diverse range of quizzes to suit every interest and level.
+                            Dive into fascinating topics, challenge your mind, and expand your horizons as you
+                            embark on an interactive learning experience like no other. From history to science,
+                            literature to pop culture, our quizzes cover it all.
+                            Join our community of learners today and let the adventure begin! 💜
+                        </p>
+                    </div>
+                </div>
+                <div className="button">
+                    <button
+                        type="submit"
+                        className="custom-button"
+                    >
+                        Start Learning
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
