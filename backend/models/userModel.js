0f1a0a092b81c1bd46b032242cd1f4a46fce988f
Lamija <lamija2002@gmail.com>
2024-05-21 23:15:23 +0200

    Did some backend, doesnt fully work yet... Problems with fetching

---
+++ backend/models/userModel.js
@@ -0,0 +1,40 @@
+const { default: mongoose } = require('mongoose')
+const mogoose = require ('mongoose')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema ({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+//static login method
+
+userSchema.statics.login = async function (email, password) {
+    if (!email || !password) {
+        throw Error ('All fields must be filelled!')
+    }
+
+    const user = await this.findOne ({email})
+
+    if (!user) {
+        throw Error ('Incorrect email!')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error ('Incorrect password!')
+    }
+
+    return user
+}
+
+module.exports = mongoose.model ('User', userSchema)
