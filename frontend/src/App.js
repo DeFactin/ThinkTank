12b3f0a24d4d2301042c0e9ef4cd472e70fa4f86
lamijazukan <zukanlamija@gmail.com>
2024-05-22 01:05:02 +0200

    resolved changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages and components
-<<<<<<< HEAD
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Flashcards from "./pages/Flashcards";
 import Quizzes from "./pages/Quizzes";
 import NewQuiz from "./pages/NewQuiz";
-=======
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
-import Flashcards from './pages/Flashcards'
-import Quizzes from './pages/Quizzes'
-import NewQuiz from './pages/NewQuiz'
-import HistoryQuiz from './pages/HistoryQuiz'
->>>>>>> ec6ce16e8b5c8a141419e92365255aeb3dc3bf28
+import HistoryQuiz from "./pages/HistoryQuiz"; // This line was missing in your branch
 
 function App() {
   return (
@@ -22,33 +14,12 @@ function App() {
         <Navbar />
         <div className="pages">
           <Routes>
-<<<<<<< HEAD
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/flashcards" element={<Flashcards />}></Route>
-            <Route path="/quizzes" element={<Quizzes />}></Route>
-            <Route path="/newquiz" element={<NewQuiz />}></Route>
-=======
-            <Route
-              path="/"
-              element={<Home />}
-            >
-            </Route>
-            <Route
-              path="/flashcards"
-              element={<Flashcards />}
-            >
-            </Route>
-            <Route
-              path="/quizzes"
-              element={<Quizzes />}
-            >
-            </Route>
-            <Route
-              path="/historyquiz"
-              element={<HistoryQuiz />}
-            >
-            </Route>
->>>>>>> ec6ce16e8b5c8a141419e92365255aeb3dc3bf28
+            <Route path="/" element={<Home />} />
+            <Route path="/flashcards" element={<Flashcards />} />
+            <Route path="/quizzes" element={<Quizzes />} />
+            <Route path="/newquiz" element={<NewQuiz />} />
+            <Route path="/historyquiz" element={<HistoryQuiz />} />{" "}
+            {/* New route */}
           </Routes>
         </div>
       </BrowserRouter>
