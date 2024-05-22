c7840f1d60219c554eb9cdef3bb01d7e20d3efdd
lamijazukan <zukanlamija@gmail.com>
2024-05-22 14:57:16 +0200

    added changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages and components
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Flashcards from "./pages/Flashcards";
-import Quizzes from "./pages/Quizzes";
-import NewQuiz from "./pages/NewQuiz";
-=======
+
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Flashcards from './pages/Flashcards'
@@ -20,12 +15,7 @@ function App() {
         <Navbar />
         <div className="pages">
           <Routes>
-<<<<<<< HEAD
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/flashcards" element={<Flashcards />}></Route>
-            <Route path="/quizzes" element={<Quizzes />}></Route>
-            <Route path="/newquiz" element={<NewQuiz />}></Route>
-=======
+
             <Route
               path="/"
               element={<Home />}
@@ -44,6 +34,11 @@ function App() {
             <Route
               path="/historyquiz"
               element={<HistoryQuiz />}
+            >
+            </Route>
+            <Route
+              path="/newquiz"
+              element={<NewQuiz/>}
             >
             </Route>
           </Routes>
